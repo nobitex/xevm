7d5d8c7628e1cbb691c10df773735395df5dac5d
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2025-01-03 02:57:17 +0330

    Simple test for opcode push

--- src/opcodes/push.rs
+++ src/opcodes/push.rs
@@ -16,15 +16,34 @@ impl<C: Context> OpcodeHandler<C> for OpcodePush {
         text: &[u8],
         _call_info: &CallInfo,
     ) -> Result<Option<ExecutionResult>, XevmError> {
-        let ahead = &text[machine.pc + 1..];
-        if ahead.len() < self.0 as usize {
+        if text.len() - machine.pc < self.0 as usize {
             return Err(XevmError::Other("Not enough bytes!".into()));
         }
-
-        machine
-            .stack
-            .push(U256::from_bytes_be(&ahead[..self.0 as usize]));
+        machine.stack.push(if self.0 == 0 {
+            U256::ZERO
+        } else {
+            U256::from_bytes_be(&text[machine.pc + 1..machine.pc + 1 + self.0 as usize])
+        });
         machine.pc += 1 + self.0 as usize;
         Ok(None)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::u256::U256;
+
+    use super::super::tests::test;
+    use super::*;
+
+    #[test]
+    fn test_opcode_push() {
+        test(
+            OpcodePush(0),
+            &[
+                (&[], Some(&[U256::from(0)])),
+                (&[U256::from(123)], Some(&[U256::from(0), U256::from(123)])),
+            ],
+        );
     }
 }
